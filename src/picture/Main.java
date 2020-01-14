bf76af84ef2edd2e54f23a3e18c787cdd9866527
Tyrell Duku <tld19@sprite37.doc.ic.ac.uk>
2020-01-14 16:24:49 +0000

    Blend and blur

--- src/picture/Main.java
+++ src/picture/Main.java
@@ -1,89 +1,56 @@
 package picture;
 
+import static picture.Process.blend;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
 
   public static void main(String[] args) {
     if (args[0].equalsIgnoreCase("invert")) {
-      invert(args[1], args[2]);
+      Process.invert(args[1], args[2]);
       System.out.println("Image has been inverted!");
+      System.out.println("Please navigate to " + args[2] + "to see it!");
     }
     if (args[0].equalsIgnoreCase("grayscale")) {
-      greyscale(args[1], args[2]);
+      Process.greyscale(args[1], args[2]);
       System.out.println("Image has been converted to greyscale!");
     }
     if (args[0].equalsIgnoreCase("rotate")) {
       if (args[1].equals("90")) {
-        rotate90(args[2], args[3]);
+        Process.rotate90(args[2], args[3]);
         System.out.println("Image has been rotated 90 degrees!");
       } else if (args[1].equals("180")) {
-        rotate180(args[2], args[3]);
+        Process.rotate180(args[2], args[3]);
         System.out.println("Image has been rotated 180 degrees!");
       } else {
-        rotate270(args[2], args[3]);
+        Process.rotate90(args[2], args[3]);
+        Process.rotate180(args[3], args[3]);
         System.out.println("Image has been rotated 270 degrees!");
       }
-    }
-  }
-
-  public static void invert(String loadedPic, String placeToSave) {
-    Picture p = Utils.loadPicture(loadedPic);
-
-    for (int i = 0; i < p.getWidth(); i++) {
-      for (int j = 0; j < p.getHeight(); j++) {
-        Color c = p.getPixel(i, j); // Initial colour (before inversion)
-        c.setBlue(255 - c.getBlue());
-        c.setRed(255 - c.getRed());
-        c.setGreen(255 - c.getGreen());
-        p.setPixel(i, j, c);
+    } else if (args[0].equalsIgnoreCase("flip")) {
+      if (args[1].equalsIgnoreCase("h")) {
+        Process.flipH(args[2], args[3]);
+      } else {
+        Process.flipV(args[2], args[3]);
       }
+    } else if (args[0].equalsIgnoreCase("blur")) {
+      Process.blend(args, args[args.length - 1]);
     }
-
-    Utils.savePicture(p, placeToSave);
   }
 
-  public static void greyscale(String loadedPic, String placeToSave) {
-    Picture p = Utils.loadPicture(loadedPic);
 
-    for (int i = 0; i < p.getWidth(); i++) {
-      for (int j = 0; j < p.getHeight(); j++) {
-        Color c = p.getPixel(i, j);
-        int sum = c.getBlue() + c.getRed() + c.getGreen();
-        int avg = sum / 3;
-        c.setRed(avg);
-        c.setBlue(avg);
-        c.setGreen(avg);
-        p.setPixel(i, j, c);
-      }
-    }
-    Utils.savePicture(p, placeToSave);
+
+  public int aRed(Color c1, Color c2) {
+    return c1.ge
   }
 
-  public static void rotate90(String loadedPic, String placeToSave) {
-    Picture p = Utils.loadPicture(loadedPic);
-    Picture rotated = Utils.createPicture(p.getHeight(), p.getWidth());
-    for (int i = 0; i < p.getWidth(); i++) {
-      for (int j = 0; j < p.getHeight(); j++) {
-        Color rotation = p.getPixel(i, j);
-        rotated.setPixel(p.getHeight() - (j + 1), i, rotation);
-      }
-    }
-    Utils.savePicture(rotated, placeToSave);
+  public static Color averageColour(Color c1, Color c2) {
+    int red1 = (c1.getRed() + c2.getRed()) / 2;
+    int blue1 = (c1.getBlue() + c2.getBlue()) / 2;
+    int green1 = (c1.getGreen() + c2.getGreen()) / 2;
+    return new Color(red1,green1,blue1);
   }
 
-  public static void rotate180(String loadedPic, String placeToSave) {
-    Picture p = Utils.loadPicture(loadedPic);
-    Picture rotated = Utils.createPicture(p.getWidth(), p.getHeight());
-    for (int i = 0; i < p.getWidth(); i++) {
-      for (int j = 0; j < p.getHeight(); j++) {
-        Color c = p.getPixel(i, j);
-        rotated.setPixel(p.getWidth() - (i + 1), p.getHeight() - (j + 1), c);
-      }
-    }
-    Utils.savePicture(rotated, placeToSave);
-  }
-
-  public static void rotate270(String loadedPic, String placeToSave) {
-    Picture p = Utils.loadPicture(loadedPic);
-    Picture rotated = Utils.createPicture(p.getHeight(), p.getWidth());
-  }
 }
--- src/picture/Process.java
+++ src/picture/Process.java
@@ -1,7 +1,125 @@
 package picture;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Process {
 
-  private Picture picture;
+ public Process() {}
 
-  // Create a Process, save it in main and then perform rotate several times...
+  public static void invert(String loadedPic, String placeToSave) {
+    Picture p = Utils.loadPicture(loadedPic);
+
+    for (int i = 0; i < p.getWidth(); i++) {
+      for (int j = 0; j < p.getHeight(); j++) {
+        Color c = p.getPixel(i, j); // Initial colour (before inversion)
+        c.setBlue(255 - c.getBlue());
+        c.setRed(255 - c.getRed());
+        c.setGreen(255 - c.getGreen());
+        p.setPixel(i, j, c);
+      }
+    }
+
+    Utils.savePicture(p, placeToSave);
+  }
+
+  public static void greyscale(String loadedPic, String placeToSave) {
+    Picture p = Utils.loadPicture(loadedPic);
+
+    for (int i = 0; i < p.getWidth(); i++) {
+      for (int j = 0; j < p.getHeight(); j++) {
+        Color c = p.getPixel(i, j);
+        int sum = c.getBlue() + c.getRed() + c.getGreen();
+        int avg = sum / 3;
+        c.setRed(avg);
+        c.setBlue(avg);
+        c.setGreen(avg);
+        p.setPixel(i, j, c);
+      }
+    }
+    Utils.savePicture(p, placeToSave);
+  }
+
+  public static void rotate90(String loadedPic, String placeToSave) {
+    Picture p = Utils.loadPicture(loadedPic);
+    Picture rotated = Utils.createPicture(p.getHeight(), p.getWidth());
+    for (int i = 0; i < p.getWidth(); i++) {
+      for (int j = 0; j < p.getHeight(); j++) {
+        Color rotation = p.getPixel(i, j);
+        rotated.setPixel(p.getHeight() - (j + 1), i, rotation);
+      }
+    }
+    Utils.savePicture(rotated, placeToSave);
+  }
+
+
+  public static void rotate180(String loadedPic, String placeToSave) {
+    Picture p = Utils.loadPicture(loadedPic);
+    Picture rotated = Utils.createPicture(p.getWidth(), p.getHeight());
+    for (int i = 0; i < p.getWidth(); i++) {
+      for (int j = 0; j < p.getHeight(); j++) {
+        Color c = p.getPixel(i, j);
+        rotated.setPixel(p.getWidth() - (i + 1), p.getHeight() - (j + 1), c);
+      }
+    }
+    Utils.savePicture(rotated, placeToSave);
+  }
+
+  public static void flipH(String loadedPic, String placeToSave) {
+    Picture original = Utils.loadPicture(loadedPic);
+    Picture newPic = Utils.createPicture(original.getWidth(), original.getHeight());
+    for (int i = 0; i < original.getWidth(); i++) {
+      for (int j = 0; j < original.getHeight(); j++) {
+        Color c = original.getPixel(i, j);
+        newPic.setPixel(original.getWidth() - (i + 1), j, c);
+      }
+    }
+    Utils.savePicture(newPic, placeToSave);
+
+  }
+
+  public static void flipV(String loadedPic, String placeToSave) {
+    Picture original = Utils.loadPicture(loadedPic);
+    Picture newPic = Utils.createPicture(original.getWidth(), original.getHeight());
+    for (int i = 0; i < original.getWidth(); i++) {
+      for (int j = 0; j < original.getHeight(); j++) {
+        Color c = original.getPixel(i, j);
+        newPic.setPixel(i, original.getHeight() - (j + 1), c);
+      }
+    }
+    Utils.savePicture(newPic, placeToSave);
+  }
+
+  public static void blend(String[] args, String placeToSave) {
+    List<String> argsList = Arrays.asList(args);
+    argsList.remove(0);
+    argsList.remove(args.length - 1); // Removes first and last elements of
+    // arguments, leaving just the image links
+
+    List<Picture> pictures = argsList.stream()
+        .map(Utils::loadPicture)
+        .collect(Collectors.toList());
+
+    int smallestW = Collections.min(pictures.stream()
+        .map(p -> (p.getWidth()))
+        .collect(Collectors.toList()));
+
+    int smallestH = Collections.min(pictures.stream()
+        .map(p -> (p.getHeight()))
+        .collect(Collectors.toList()));
+
+    Picture p = Utils.createPicture(smallestW, smallestH);
+
+    for (int i = 0; i < smallestW; i++) {
+      for (int j = 0; j < smallestH; j++) {
+        List <Color> cols = pictures.stream()
+            .map(k -> k.getPixel(i,j))
+            .collect(Collectors.toList());
+      }
+    }
+
+  }
+
+}
