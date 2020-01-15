330a8289e2d74ddbd2761be15c7f5d2db337163f
Duku, Tyrell <tyrell.duku19@imperial.ac.uk>
2020-01-15 18:46:41 +0000

    Replace Main.java

--- src/picture/Main.java
+++ src/picture/Main.java
@@ -1,44 +1,65 @@
 package picture;
 
-import static picture.Process.blend;
-
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class Main {
 
   public static void main(String[] args) {
-    if (args[0].equalsIgnoreCase("invert")) {
-      Process.invert(args[1], args[2]);
-      System.out.println("Image has been inverted!");
-      System.out.println("Please navigate to " + args[2] + "to see it!");
-    }
-    if (args[0].equalsIgnoreCase("grayscale")) {
-      Process.greyscale(args[1], args[2]);
-      System.out.println("Image has been converted to greyscale!");
-    }
-    if (args[0].equalsIgnoreCase("rotate")) {
-      if (args[1].equals("90")) {
-        Process.rotate90(args[2], args[3]);
-        System.out.println("Image has been rotated 90 degrees!");
-      } else if (args[1].equals("180")) {
-        Process.rotate180(args[2], args[3]);
-        System.out.println("Image has been rotated 180 degrees!");
-      } else {
-        Process.rotate90(args[2], args[3]);
-        Process.rotate180(args[3], args[3]);
-        System.out.println("Image has been rotated 270 degrees!");
-      }
-    } else if (args[0].equalsIgnoreCase("flip")) {
-      if (args[1].equalsIgnoreCase("h")) {
-        Process.flipH(args[2], args[3]);
-      } else {
-        Process.flipV(args[2], args[3]);
-      }
-    } else if (args[0].equalsIgnoreCase("blur")) {
-      Process.blend(args, args[args.length - 1]);
+
+    switch (args[0]) {
+      case "invert":
+        Process.invert(args);
+        System.out.println("Image has been inverted!");
+        System.out.println("Please navigate to " + args[2] + " to see it!");
+        break;
+
+      case "grayscale":
+        Process.greyscale(args);
+        System.out.println("Image has been converted to greyscale!");
+        System.out.println("Please navigate to " + args[2] + " to see it!");
+        break;
+
+      case "rotate":
+        switch (args[1]) {
+          case "90":
+            Process.rotate90(args);
+            System.out.println("Image has been rotated 90 degrees!");
+            System.out.println("Please navigate to " + args[2] + " to see it!");
+            break;
+          case "180":
+            Process.rotate180(args[2], args[3]);
+            System.out.println("Image has been rotated 180 degrees!");
+            System.out.println("Please navigate to " + args[3] + " to see it!");
+            break;
+          default:
+            Process.rotate90(args);
+            Process.rotate180(args[3], args[3]);
+            System.out.println("Image has been rotated 270 degrees!");
+            break;
+        }
+        break;
+
+      case "flip":
+        switch (args[1]) {
+          case "H":
+            Process.flipH(args[2], args[3]);
+            System.out.println("Image has been flipped horizontally!");
+            break;
+          default:
+            Process.flipV(args[2], args[3]);
+            System.out.println("Image has been flipped vertically!");
+        }
+        break;
+
+      case "blend":
+        Process.blend(args);
+        System.out.print("Image has been blended with " + (args.length - 2));
+        System.out.print(" images! Please navigate to " + args[args.length - 1]);
+        System.out.print(" to see it!");
+        break;
+
+      default:
+        Process.blur(args);
+        System.out.println("Image has been blurred!");
+        System.out.println("Please navigate to " + args[2] + " to see it!");
     }
   }
-
-
 }
