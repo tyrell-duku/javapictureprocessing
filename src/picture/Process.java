8dd91999f016e2815dbf27dc40f4913c0033313f
Duku, Tyrell <tyrell.duku19@imperial.ac.uk>
2020-01-16 14:33:18 +0000

    Update src/picture/Process.java

--- src/picture/Process.java
+++ src/picture/Process.java
@@ -1,6 +1,9 @@
 package picture;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Process {
 
